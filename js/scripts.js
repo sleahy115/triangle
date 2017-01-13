409e2092148c84d358ce224bac04c963cb8fd5f6
sarah leahy <sarah leahy>
2017-01-12 17:11:40 -0700

    add working form --shows resuts for different triangles

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $("#sides form").submit(function(event) {
+  $("#numbers form").submit(function(event) {
     var number1Input = $("input#side1").val();
     var number2Input = $("input#side2").val();
     var number3Input = $("input#side3").val();
@@ -13,9 +13,6 @@ $(document).ready(function(){
     else if (number1Input !== number2Input || number3Input ){
     $("#scalene").show();
     }
-    // else if (side1Input + side2Input <= side3Input) {
-    //   $("#not").show();
-    // }
 
     event.preventDefault();
   });
